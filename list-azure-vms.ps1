2a549fb5bbdca7d92d7934d54bffb2fb4726b95a
Ramon <ramonpichardo+github@gmail.com>
2020-06-01 14:56:43 -0700

    Prompt for and set Azure subscription

--- list-azure-vms.ps1
+++ list-azure-vms.ps1
@@ -7,6 +7,11 @@ Connect-AzAccount -Credential $credential
 # List all Azure subscriptions in the current tenant
 Get-AzSubscription
 
+# Set Azure subscription
+$subscription = Read-Host -Prompt 'Input the Azure subscription name to be queried'
+$context = Get-AzSubscription -SubscriptionName $subscription
+Set-AzContext $context
+
 # List Azure VMs, sorted alphabetically by VM name
 Get-AzVM -status | Sort-Object -Property Name | Select-Object Name,ResourceGroupName,
   @{Name="VmSize"; Expression={$_.HardwareProfile.VmSize}},
